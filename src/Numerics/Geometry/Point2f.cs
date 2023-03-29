6afecee928d4f94faa17b944e7508ba21607cde4
RedRing <info@redring.jp>
2023-03-29 19:27:05 +0900

    2次元単精度Point構造体の追加及びUtilityの初期作成

---
+++ src/Numerics/Geometry/Point2f.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numerics
+{
+    [Serializable]
+    [StructLayout(LayoutKind.Sequential)]
+    public struct Point2f
+    {
+        public float X { get; set; }
+
+        public float Y { get; set; }
+
+        public Point2f(float x, float y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
---
+++ src/Numerics/MathHelper.cs
@@ -0,0 +1,11 @@
+﻿namespace Numerics
+{
+    public static class MathHelper
+    {
+        internal static double Epsilon = 0.0001;
+
+        public static double Sqrt(double d) => Math.Sqrt(d);
+
+        public const double PI = Math.PI;
+    }
+}
